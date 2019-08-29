77fd22ddfc398f3d8857a6066ac1effc240d02bf
Nirlipta Prasoon Gaur <ngaur@euronetworldwide.com>
2019-08-29 17:03:14 +0530

    Minor fixes

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -22,6 +22,14 @@ public class MathCalculator extends CordovaPlugin {
             this.add(args, callbackContext);
             return true;
         }
+        else if (action.equals("multiply")){
+            this.multiply(args, callbackContext);
+            return true;
+        }
+        else if (action.equals("divide")){
+            this.divide(args, callbackContext);
+            return true;
+        }
         return false;
     }
 
@@ -50,4 +58,32 @@ public class MathCalculator extends CordovaPlugin {
         }
         else{  callback.error("Please do not pass null values");  }
     }
+
+    private void multiply(JSONArray args, CallbackContext callback){
+        if(args!= null){
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success(""+(p1*p2));
+            } catch(Exception e){
+                callback.error("Something went wrong "+ e);
+            }
+        }
+        else{  callback.error("Please do not pass null values");  }
+    }
+
+
+    private void divide(JSONArray args, CallbackContext callback){
+        if(args!= null){
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success(""+(p1/p2));
+            } catch(Exception e){
+                callback.error("Something went wrong "+ e);
+            }
+        }
+        else{  callback.error("Please do not pass null values");  }
+    }
+
 }
--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -4,6 +4,14 @@ module.exports.substract = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'substract', [arg0]);
 };
 
-module.exports.add = function (arg0, success, error){
-    exec( success, error, 'MathCalculator', 'add', [arg0]);
+module.exports.add = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'add', [arg0]);
+}
+
+module.exports.multiply = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'multiply', [arg0]);
+}
+
+module.exports.divide = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'divide', [arg0]);
 }
